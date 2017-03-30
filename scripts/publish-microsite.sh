774a4fabdf671625ddb06a55fcc23fab92d5bf07
Mark <mail@markdejong.org>
2017-03-30 22:36:39 +0200

    Remove docker-compose

--- scripts/publish-microsite.sh
+++ scripts/publish-microsite.sh
@@ -5,8 +5,4 @@ git config --global user.email "mail@markdejong.org"
 git config --global user.name "Mark"
 git config --global push.default simple
 
-docker-compose up -d
-
 sbt docs/publishMicrosite
-
-docker-compose down
